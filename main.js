ee9e8120d2f8dc9b2fa10d53cc9f91bdfd7b5f67
Liam Doherty <lfdoherty@gmail.com>
2013-08-06 06:50:37 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var self = module.exports = global.page || {}
+self.server = false
+
+self.ready = function(cb){
+	var hasLoaded = document.readyState === 'interactive' || document.readyState === 'complete'
+	if(hasLoaded){
+		cb()
+	}else{
+		document.addEventListener('DOMContentLoaded', cb)
+	}
+}
+
+self.setBodyHtml = function(str){
+	document.body.innerHTML = str
+}
+
+self.on = function(eventName, cb){
+	//throw new Error('TODO')
+	document.addEventListener(eventName, cb)
+}
